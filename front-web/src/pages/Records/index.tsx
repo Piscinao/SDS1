35c4ca12646c8a18a2d275879788042f19ef6280
Piscinao <henhvj@gmail.com>
2020-09-19 18:07:41 -0300

    Criando página de listagem dos registros

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,40 +1,87 @@
-import React from 'react';
-
-import { ReactComponent as ArrowIcon } from '../../assets/arrow.svg';
-import { ReactComponent as Gamer } from '../../assets/gamer.svg';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './styles.css';
+import { RecordsResponse } from './types';
+import { formatDate } from './helpers';
+import Pagination from './Pagination';
+import { Link } from 'react-router-dom';
 
-const Records = () => (
-  <div className="home-container">
-    <div className="home-text">
-      <h1 className="home-text-title">
-        Quais jogos a galera gosta mais?
-      </h1>
+const BASE_URL = 'http://localhost:8080';
 
-      <h3 className="home-text-subtitle">
-        Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo
-      </h3>
+const Records = () => {
 
-      <div className="home-actions">
-        <button className="home-btn">
-          QUERO SABER QUAIS asaaaa
-        </button>
 
-        <div className="home-btn-icon">
-            <ArrowIcon />
-        </div>
+  // use state usa o estado interno do componente é um Rook 
+  // armazena um valor para consumir o valor no componente
+  const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
+  const [activePage, setActivePage] = useState(0);
 
-        
+  console.log(recordsResponse);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/records?linesPerPage=12&page=${activePage}`)
+      .then(response => setRecordsResponse(response.data));
+  }, [activePage]);
+
+  const handlePageChange = (index: number) => {
+    setActivePage(index)
+  }
+
+  return (
+    <div className="page-container">
+      <div className="filters-container records-actions">
+        <Link to="/charts">
+          <button className="action-filters">
+            Ver Gráficos
+          </button>
+        </Link>
+
       </div>
+      <table className="records-table" cellPadding="0" cellSpacing="0">
+        <thead>
+          <tr>
+            <th>INSTANTE</th>
+            <th>NOME</th>
+            <th>IDADE</th>
+            <th>PLATAFORMA</th>
+            <th>GÊNERO</th>
+            <th>TÍTULO DO GAME</th>
+          </tr>
+        </thead>
 
-    
+        <tbody>
+          {recordsResponse?.content.map(record => (
+
+            <tr key={record.id}>
+              <td>{formatDate(record.moment)}</td>
+              <td>{record.name}</td>
+              <td>{record.age}</td>
+              <td className="text-secondary">{record.gamePlatform}</td>
+              <td>{record.genreName}</td>
+              <td className="text-primary">{record.gameTitle}</td>
+            </tr>
+
+
+          ))}
+
+
+
+
+        </tbody>
+
+
+      </table>
+
+
+      <Pagination 
+      activePage={activePage}
+      goToPage={handlePageChange}
+      totalPages={recordsResponse?.totalPages}
+      
+      />
 
     </div>
-
-    <Gamer className="home-image" />
-    
-  </div>
-);
+  );
+}
 
 export default Records;
